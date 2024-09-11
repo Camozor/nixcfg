2ed8e7fd4c44d61bafc143e0f08487ae5210bd19
Camille <camille.vauchel@gmail.com>
2024-09-11 19:00:39 +0200

    Temp

--- configuration.nix
+++ configuration.nix
@@ -34,10 +34,16 @@ in {
   '';
 
   programs.hyprland = {
-    # Install the packages from nixpkgs
     enable = true;
-    # Whether to enable XWayland
     xwayland.enable = true;
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   # Enable networking
@@ -119,9 +125,6 @@ in {
     air
     cargo
     rustc
-    i3lock
-    i3blocks
-    betterlockscreen
     pavucontrol
     feh
     jq
@@ -149,9 +152,22 @@ in {
     python3
     vlc
     obsidian
+
+    hyprland
+    swww
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+    xwayland
+    meson
+    wayland-protocols
+    wayland-utils
+    wl-clipboard
+    wlroots
+    rofi-wayland
+    wofi
   ];
 
-  environment.wordlist.enable = true;
+  fonts.fonts = with pkgs; [ nerdfonts meslo-lgs-nf ];
 
   documentation.dev.enable = true;
 
