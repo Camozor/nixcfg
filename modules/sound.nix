156766a25592082696e35208cdf52cc9d59e0093
Camille <camille.vauchel@gmail.com>
2024-09-23 14:33:57 +0200

    Sound

--- modules/sound.nix
+++ modules/sound.nix
@@ -13,7 +13,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [ pamixer pavucontrol vlc spotify ];
+  environment.systemPackages = with pkgs; [
+    pamixer
+    pavucontrol
+    pulseaudio
+    vlc
+    spotify
+  ];
 }
