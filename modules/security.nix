2e1969aa52aa4ab9ee345af44541a0ad8ea2bf80
Camille Vauchel <camille.vauchel@neomi.immo>
2025-03-31 17:39:48 +0200

    Remove useless comment

--- modules/security.nix
+++ modules/security.nix
@@ -3,10 +3,8 @@
 {
   programs._1password = { enable = true; };
 
-  # Enables the 1Password desktop app
   programs._1password-gui = {
     enable = true;
-    # this makes system auth etc. work properly
     polkitPolicyOwners = [ "camille" ];
   };
 }
