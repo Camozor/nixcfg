94d76cde6878c13b51a6aa3e7cb3218eecb49916
Camille Vauchel <camille.vauchel@neomi.immo>
2025-07-31 15:55:23 +0200

    tms

--- modules/work.nix
+++ modules/work.nix
@@ -7,6 +7,7 @@
     127.0.0.1 neomi
   '';
   networking.enableIPv6 = false;
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = "0";
 
   environment.systemPackages = with pkgs; [ teams-for-linux azure-cli ];
 }
