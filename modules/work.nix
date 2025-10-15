6309a3f005255357ce158050afabdbe92fc2ef90
Camille Vauchel <camille.vauchel@neomi.immo>
2025-10-15 09:47:38 +0200

    ipv6 on

--- modules/work.nix
+++ modules/work.nix
@@ -6,7 +6,6 @@
     127.0.0.1 kamineo
     127.0.0.1 neomi
   '';
-  networking.enableIPv6 = false;
   boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = "0";
 
   environment.systemPackages = with pkgs; [ teams-for-linux azure-cli ];
