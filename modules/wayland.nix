9a1b1519b075486a536d449b0cc7917e4efe2def
Camille <camille.vauchel@gmail.com>
2025-11-30 16:01:36 +0100

    Working gaming stuff

--- modules/wayland.nix
+++ modules/wayland.nix
@@ -4,9 +4,9 @@
   services.xserver = {
     enable = true;
     xkb.layout = "fr";
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   xdg.portal = {
     enable = true;
@@ -15,20 +15,44 @@
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
-  };
 
   programs.steam = {
     enable = true;
     extraPackages = with pkgs; [ mangohud gamemode ];
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
-  programs.steam.extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  environment.systemPackages = with pkgs; [
+    steam-run
+    heroic-unwrapped
 
-  environment.systemPackages = with pkgs; [ steam-run ];
+    freetype
+    fontconfig
+    xorg.libXrender
+    xorg.libXext
 
-  hardware.opengl.enable = true;
+    wineWowPackages.waylandFull
+    winetricks
+
+    vulkan-tools
+    pkgsi686Linux.vulkan-tools
+
+    gamemode
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [ vulkan-loader ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [ vulkan-loader ];
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    powerManagement.enable = true;
+    package = pkgs.linuxPackages.nvidiaPackages.stable;
+  };
+
 }
